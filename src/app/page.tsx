1e2200bf41e30663ccc912eb89b2f27ce0097455
Alexandre Rayol <alexandrerayol.dev@gmail.com>
2024-02-20 17:17:01 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { CharactersList } from "@/components/charactersList";
-import { Header } from "@/components/header";
-import { SkeletonCard } from "@/components/skeletonCard";
 
 export default function Home() {
   return (
